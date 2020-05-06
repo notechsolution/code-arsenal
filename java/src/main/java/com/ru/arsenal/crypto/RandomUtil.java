4008a58cd250d070549966f3d3e69920b4e3082e
notechsolution <lezhi.zeng@163.com>
2020-05-07 06:51:10 +0800

    [crypto-random] implement RandomUtil

--- java/src/main/java/com/ru/arsenal/crypto/RandomUtil.java
+++ java/src/main/java/com/ru/arsenal/crypto/RandomUtil.java
@@ -1,6 +1,7 @@
 package com.ru.arsenal.crypto;
 
 import java.util.Random;
+import org.apache.commons.lang3.RandomStringUtils;
 
 public class RandomUtil {
     public static String ALPHA = "abcdefghijkllmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -8,14 +9,19 @@ public class RandomUtil {
     public static String ALPHA_NUMBERIC = "abcdefghijkllmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     public static void main(String[] args) {
-        System.out.println("--- Start RandomUtils ---");
-        for (int i = 0; i < 100; i++) {
-            System.out.println(RandomUtil.generateAlphaString(10));
+        System.out.println("--- Generate 10 random String using Random() ---");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtil.generate_alphaString_using_Random(32));
+        }
+
+        System.out.println("\n--- Generate 10 random String using apache.RandomStringUtils ---");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RandomUtil.generate_alphaString_using_RandomUtils(32));
         }
 
     }
 
-    public static String generateAlphaString(int length) {
+    public static String generate_alphaString_using_Random(int length) {
         Random seed = new Random();
         StringBuffer buffer = new StringBuffer();
         int bound = ALPHA.length();
@@ -23,5 +29,10 @@ public class RandomUtil {
             buffer.append(ALPHA.charAt(seed.nextInt(bound)));
         }
         return buffer.toString();
+    }
+
+    // using RandomStringUtils. which actually use Random() in the background
+    public static String generate_alphaString_using_RandomUtils(int length){
+       return RandomStringUtils.randomAlphabetic(length);
     }
 }
