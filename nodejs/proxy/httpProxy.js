091c42848891378a559f6ec2736c6b9c83fe5dd1
notechsolution <notech@126.com>
2021-08-27 09:52:42 +0800

    [proxy] enhance the logs

--- nodejs/proxy/httpProxy.js
+++ nodejs/proxy/httpProxy.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const net = require('net');
 const {URL} = require('url');
+const requestIP = require('request-ip');
+const _ = require('lodash');
 const access = console;
 const error = console;
 
@@ -13,36 +15,50 @@ function request(srcReq, srcRes) {
         method: srcReq.method,
         headers: srcReq.headers
     }
-    let logLine = `${new Date().toISOString()} ${srcReq.method} ${srcReq.url}`
+    // timestamp clientAddress requestMethod URI result/statusCodes responseTime
+    const startAt = process.hrtime();
+    let logLine = `${Date.now()} ${requestIP.getClientIp(srcReq)} ${srcReq.method} ${srcReq.url}`
     const destRequest = http.request(options, destResponse => {
         srcRes.writeHead(destResponse.statusCode, destResponse.headers);
         destResponse.pipe(srcRes)
-        logLine += ` ${destResponse.statusCode}`
+        logLine += ` ${destResponse.statusCode} ${_.get(destResponse.headers,"content-type",'-')} ${_getTotalTime(startAt)}`
+        access.log(logLine);
     }).on('error', e => {
-
         srcRes.end();
-        logLine += ' ERROR';
-        error.log(logLine+'\r\n'+ e)
+        logLine += ` ERROR ${_getTotalTime(startAt)}`;
+        error.log(logLine + '\r\n' + e)
     })
-    access.log(logLine);
-
     srcReq.pipe(destRequest);
 }
 
-function connect(srcReq, srcSocket){
+function _getTotalTime(startAt){
+    // time elapsed from request start
+    const elapsed = process.hrtime(startAt)
+
+    // cover to milliseconds
+    const ms = (elapsed[0] * 1e3) + (elapsed[1] * 1e-6)
+
+    // return truncated value
+    return ms.toFixed(3)
+}
+function connect(srcReq, srcSocket) {
     const url = new URL(`http://${srcReq.url}`);
-    let logLine = `${new Date().toISOString()} CONNECT ${url.hostname}:${url.port}`
-    const destSocket = net.connect(parseInt(url.port), url.hostname, ()=>{
+    const startAt = process.hrtime();
+    let logLine = `${Date.now()} CONNECT ${url.hostname}:${url.port}`
+    const destSocket = net.connect(parseInt(url.port), url.hostname, () => {
         srcSocket.write('HTTP/1.1 200 Connection Established\r\n\r\n');
         destSocket.pipe(srcSocket);
+        logLine += ` ${_getTotalTime(startAt)}`
+        access.log(logLine);
     }).on('error', e => {
         srcSocket.end();
-        logLine += ' ERROR';
-        error.log(logLine+'\r\n'+ e)
+        logLine += ` ERROR ${_getTotalTime(startAt)}`;
+        error.log(logLine + '\r\n' + e)
     })
-    access.log(logLine);
+
     srcSocket.pipe(destSocket);
 }
+
 const PORT = parseInt(process.env.PORT) || 8888;
 console.log(`Starting My Proxy...`)
 http.createServer()
